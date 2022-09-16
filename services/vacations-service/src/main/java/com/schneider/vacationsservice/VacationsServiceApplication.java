1df926cd973ffbb7deb207d94b704188808ef115
Digitory-2022 <howeda1667@5k2u.com>
2022-09-16 15:40:27 +0200

    add skaffold and project structure

---
+++ services/vacations-service/src/main/java/com/schneider/vacationsservice/VacationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.schneider.vacationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationsServiceApplication.class, args);
+	}
+
+}
